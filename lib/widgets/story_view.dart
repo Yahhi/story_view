50ad3f5d066a733510c9f592cb267cc7f394492d
Ilya Drats <ybhyhen@gmail.com>
2021-04-14 17:21:30 +0300

    Fixes of StoryItem.text

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -13,9 +13,11 @@ Widget _bluredContentWrapper(Widget content, {double blurEffect = 0}) {
   if (blurEffect > 0) {
     return Positioned.fill(
         child: ClipRect(
-          child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: blurEffect, sigmaY: blurEffect), child: content),
-        ));
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: blurEffect, sigmaY: blurEffect),
+        child: content,
+      ),
+    ));
   }
 
   return content;
@@ -67,8 +69,8 @@ class StoryItem {
   /// what inline/full-page means.
 
   static StoryItem text({
-    @required String title,
-    @required Color backgroundColor,
+    String title,
+    Color backgroundColor,
     Key key,
     TextStyle textStyle,
     bool shown = false,
@@ -76,21 +78,25 @@ class StoryItem {
     bool roundedBottom = false,
     Widget hapticContent,
     Duration duration,
+    Gradient gradient,
   }) {
-    final double contrast = ContrastHelper.contrast([
-      backgroundColor.red,
-      backgroundColor.green,
-      backgroundColor.blue,
-    ], [
-      255,
-      255,
-      255
-    ] /** white text */);
+    final double contrast = backgroundColor == null
+        ? 2
+        : ContrastHelper.contrast([
+            backgroundColor.red,
+            backgroundColor.green,
+            backgroundColor.blue,
+          ], [
+            255,
+            255,
+            255
+          ] /** white text */);
 
     return StoryItem(
       Container(
         key: key,
         decoration: BoxDecoration(
+          gradient: gradient,
           color: backgroundColor,
           borderRadius: BorderRadius.vertical(
             top: Radius.circular(roundedTop ? 8 : 0),
@@ -102,17 +108,19 @@ class StoryItem {
           vertical: 16,
         ),
         child: Center(
-          child: Text(
-            title,
-            style: textStyle?.copyWith(
-                  color: contrast > 1.8 ? Colors.white : Colors.black,
-                ) ??
-                TextStyle(
-                  color: contrast > 1.8 ? Colors.white : Colors.black,
-                  fontSize: 18,
+          child: title?.isEmpty ?? true
+              ? const SizedBox.shrink()
+              : Text(
+                  title,
+                  style: textStyle?.copyWith(
+                        color: contrast > 1.8 ? Colors.white : Colors.black,
+                      ) ??
+                      TextStyle(
+                        color: contrast > 1.8 ? Colors.white : Colors.black,
+                        fontSize: 18,
+                      ),
+                  textAlign: TextAlign.center,
                 ),
-            textAlign: TextAlign.center,
-          ),
         ),
         //color: backgroundColor,
       ),
@@ -161,7 +169,8 @@ class StoryItem {
                         horizontal: 24,
                         vertical: 8,
                       ),
-                      color: caption != null ? Colors.black54 : Colors.transparent,
+                      color:
+                          caption != null ? Colors.black54 : Colors.transparent,
                       child: caption != null
                           ? Text(
                               caption,
@@ -176,9 +185,11 @@ class StoryItem {
                   ),
                 ),
                 blurEffect: blurEffect),
-                if (gradient != null) Positioned.fill(child: Container(
-                  decoration: BoxDecoration(gradient: gradient),
-                ))
+            if (gradient != null)
+              Positioned.fill(
+                  child: Container(
+                decoration: BoxDecoration(gradient: gradient),
+              ))
           ],
         ),
       ),
@@ -275,7 +286,8 @@ class StoryItem {
                     width: double.infinity,
                     margin: EdgeInsets.only(bottom: 24),
                     padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-                    color: caption != null ? Colors.black54 : Colors.transparent,
+                    color:
+                        caption != null ? Colors.black54 : Colors.transparent,
                     child: caption != null
                         ? Text(
                             caption,
@@ -333,7 +345,8 @@ class StoryItem {
                       horizontal: 24,
                       vertical: 8,
                     ),
-                    color: caption != null ? Colors.black54 : Colors.transparent,
+                    color:
+                        caption != null ? Colors.black54 : Colors.transparent,
                     child: caption != null
                         ? Text(
                             caption,
@@ -457,7 +470,8 @@ class StoryView extends StatefulWidget {
     this.onHorizontalSwipeForward,
     this.onHorizontalSwipeBackward,
     this.onVerticalSwipeComplete,
-  })  : assert(storyItems != null && storyItems.length > 0, "[storyItems] should not be null or empty"),
+  })  : assert(storyItems != null && storyItems.length > 0,
+            "[storyItems] should not be null or empty"),
         assert(progressPosition != null, "[progressPosition] cannot be null"),
         assert(
           repeat != null,
@@ -480,9 +494,10 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
 
   VerticalDragInfo verticalDragInfo;
 
-  StoryItem get _currentStory => widget.storyItems.firstWhere((it) => !it.shown, orElse: () => null);
-  StoryItem get _currentOrLastStory =>
-      widget.storyItems.firstWhere((it) => !it.shown, orElse: () => widget.storyItems.last);
+  StoryItem get _currentStory =>
+      widget.storyItems.firstWhere((it) => !it.shown, orElse: () => null);
+  StoryItem get _currentOrLastStory => widget.storyItems
+      .firstWhere((it) => !it.shown, orElse: () => widget.storyItems.last);
 
   Widget get _currentView => _currentOrLastStory.view;
 
@@ -510,7 +525,8 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
       });
     }
 
-    this._playbackSubscription = widget.controller.playbackNotifier.listen((playbackStatus) {
+    this._playbackSubscription =
+        widget.controller.playbackNotifier.listen((playbackStatus) {
       switch (playbackStatus) {
         case PlaybackState.play:
           _removeNextHold();
@@ -565,7 +581,8 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
       widget.onStoryShow(storyItem);
     }
 
-    _animationController = AnimationController(duration: storyItem.duration, vsync: this);
+    _animationController =
+        AnimationController(duration: storyItem.duration, vsync: this);
 
     _animationController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -579,7 +596,8 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
       }
     });
 
-    _currentAnimation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    _currentAnimation =
+        Tween(begin: 0.0, end: 1.0).animate(_animationController);
 
     widget.controller.play();
   }
@@ -663,9 +681,11 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
     return GestureDetector(
       onHorizontalDragEnd: widget.enableHorizontalSwipe
           ? (details) {
-              if (details.primaryVelocity > 0 && widget.onHorizontalSwipeBackward != null) {
+              if (details.primaryVelocity > 0 &&
+                  widget.onHorizontalSwipeBackward != null) {
                 widget.onHorizontalSwipeBackward();
-              } else if (details.primaryVelocity < 0 && widget.onHorizontalSwipeForward != null) {
+              } else if (details.primaryVelocity < 0 &&
+                  widget.onHorizontalSwipeForward != null) {
                 widget.onHorizontalSwipeForward();
               }
             }
@@ -676,7 +696,9 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
           children: <Widget>[
             _currentView,
             Align(
-              alignment: widget.progressPosition == ProgressPosition.top ? Alignment.topCenter : Alignment.bottomCenter,
+              alignment: widget.progressPosition == ProgressPosition.top
+                  ? Alignment.topCenter
+                  : Alignment.bottomCenter,
               child: SafeArea(
                 bottom: widget.inline ? false : true,
                 // we use SafeArea here for notched and bezeles phones
@@ -686,10 +708,14 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                     vertical: 8,
                   ),
                   child: PageBar(
-                    widget.storyItems.map((it) => PageData(it.duration, it.shown)).toList(),
+                    widget.storyItems
+                        .map((it) => PageData(it.duration, it.shown))
+                        .toList(),
                     this._currentAnimation,
                     key: UniqueKey(),
-                    indicatorHeight: widget.inline ? IndicatorHeight.small : IndicatorHeight.large,
+                    indicatorHeight: widget.inline
+                        ? IndicatorHeight.small
+                        : IndicatorHeight.large,
                   ),
                 ),
               ),
@@ -738,8 +764,10 @@ class StoryViewState extends State<StoryView> with TickerProviderStateMixin {
                       : (details) {
                           widget.controller.play();
                           // finish up drag cycle
-                          if (!verticalDragInfo.cancel && widget.onVerticalSwipeComplete != null) {
-                            widget.onVerticalSwipeComplete(verticalDragInfo.direction);
+                          if (!verticalDragInfo.cancel &&
+                              widget.onVerticalSwipeComplete != null) {
+                            widget.onVerticalSwipeComplete(
+                                verticalDragInfo.direction);
                           }
 
                           verticalDragInfo = null;
@@ -814,7 +842,8 @@ class PageBarState extends State<PageBar> {
   }
 
   bool isPlaying(PageData page) {
-    return widget.pages.firstWhere((it) => !it.shown, orElse: () => null) == page;
+    return widget.pages.firstWhere((it) => !it.shown, orElse: () => null) ==
+        page;
   }
 
   @override
@@ -823,10 +852,12 @@ class PageBarState extends State<PageBar> {
       children: widget.pages.map((it) {
         return Expanded(
           child: Container(
-            padding: EdgeInsets.only(right: widget.pages.last == it ? 0 : this.spacing),
+            padding: EdgeInsets.only(
+                right: widget.pages.last == it ? 0 : this.spacing),
             child: StoryProgressIndicator(
               isPlaying(it) ? widget.animation.value : (it.shown ? 1 : 0),
-              indicatorHeight: widget.indicatorHeight == IndicatorHeight.large ? 5 : 3,
+              indicatorHeight:
+                  widget.indicatorHeight == IndicatorHeight.large ? 5 : 3,
             ),
           ),
         );
@@ -845,7 +876,8 @@ class StoryProgressIndicator extends StatelessWidget {
   StoryProgressIndicator(
     this.value, {
     this.indicatorHeight = 5,
-  }) : assert(indicatorHeight != null && indicatorHeight > 0, "[indicatorHeight] should not be null or less than 1");
+  }) : assert(indicatorHeight != null && indicatorHeight > 0,
+            "[indicatorHeight] should not be null or less than 1");
 
   @override
   Widget build(BuildContext context) {
@@ -875,7 +907,9 @@ class IndicatorOval extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final paint = Paint()..color = this.color;
     canvas.drawRRect(
-        RRect.fromRectAndRadius(Rect.fromLTWH(0, 0, size.width * this.widthFactor, size.height), Radius.circular(3)),
+        RRect.fromRectAndRadius(
+            Rect.fromLTWH(0, 0, size.width * this.widthFactor, size.height),
+            Radius.circular(3)),
         paint);
   }
 
@@ -890,13 +924,16 @@ class ContrastHelper {
   static double luminance(int r, int g, int b) {
     final a = [r, g, b].map((it) {
       final value = it.toDouble() / 255.0;
-      return value <= 0.03928 ? value / 12.92 : pow((value + 0.055) / 1.055, 2.4);
+      return value <= 0.03928
+          ? value / 12.92
+          : pow((value + 0.055) / 1.055, 2.4);
     }).toList();
 
     return (a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722).toDouble();
   }
 
   static double contrast(List<int> rgb1, List<int> rgb2) {
-    return luminance(rgb2[0], rgb2[1], rgb2[2]) / luminance(rgb1[0], rgb1[1], rgb1[2]);
+    return luminance(rgb2[0], rgb2[1], rgb2[2]) /
+        luminance(rgb1[0], rgb1[1], rgb1[2]);
   }
 }
